460c2900faa6b99b8cf8a5cadad0ccb80fc92e17
coskun unen <coskunen@gmail.com>
2023-03-27 22:14:52 +0300

    completed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Karakter from './components/Karakter'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -7,10 +9,22 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-
+  const [charactersData , setCharactersData] = useState([]);
+useEffect(()=>{
+  axios
+  .get("https://swapi.dev/api/people/")
+  .then((res)=> res.data)
+  .then((data)=>{
+    console.log("data cekildi >", data);
+    setCharactersData(data);
+  })
+}, []);
   return (
     <div className="App">
-      <h1 className="Header">Karakterler</h1>
+     {charactersData && charactersData.map((char)=>{
+      return <Karakter char ={char}/>
+     })}
+      
     </div>
   );
 }
--- src/components/Karakter.js
+++ src/components/Karakter.js
@@ -1,1 +1,19 @@
 // Karakter bileşeniniz buraya gelecek
+
+import { useState } from "react";
+
+export default function(char){
+    const [expanded , setExpanded] = useState(false);
+
+    const handleChange = (panel) => (event, isExpanded) =>{
+            setExpanded(isExpanded ? panel : false);
+    
+    }
+    return(
+        <div>
+            {char.name}
+            Name : {char.name}
+            Height : {char.height}
+        </div>
+    );
+}
